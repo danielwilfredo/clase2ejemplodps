d3c16b1ce82def6f2ad5d2e02702283908d4084e
danielwilfredo <xvwilito11@gmail.com>
2021-08-09 10:58:50 -0600

    cambios realizados en ejemplo clase 2 dps

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import {useState, useEffect} from 'react';
+import './App.css';
+
+
+function App() {
+  const [dolar, setDolar] = useState(0);
+  const [euro, setEuro] = useState(0);
+  const [bitcoin, setBitcoin] = useState(0);
+
+
+  const [tipo, setTipo] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    setDolar((total * tipo));
+    setEuro((total * tipo) / 1.17);
+    setBitcoin((total * tipo) / 45764.60);
+  }, [total, tipo]);
+
+
+  const handleTotalChange = e => {
+    if (!isNaN(e.target.value)) {
+      setTotal(e.target.value);
+    }
+  };
+
+
+  return (
+    <div className="App">
+      <h1>Convertidor de divisas (Dolar, Euro, Bitcoin)</h1>
+
+      <p>Dolar: {dolar}</p>
+      <p>Euro: {euro}</p>
+      <p>Bitcoins: {bitcoin}</p>
+      <hr />
+
+      <h3>{total}</h3>
+
+      <select onChange={event => setTipo(event.target.value)} value={tipo}>
+        <option value={1}>Dolar</option>
+        <option value={1.17}>Euro</option>
+        <option value={45764.60}>bitcoin</option>
+      </select>
+      <input onChange={handleTotalChange} value={total} />
+    </div>
+  );
+}
+
+export default App;
